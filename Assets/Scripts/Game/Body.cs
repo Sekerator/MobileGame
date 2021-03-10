b411746cd6663a1a52cf202dafdd3d52dcd8a556
Sekerator <tagieinullaev@mail.ru>
2021-03-10 05:55:37 +0300

    Add class: Engine Body Chassis

--- Assets/Scripts/Game/Body.cs
+++ Assets/Scripts/Game/Body.cs
@@ -6,6 +6,16 @@ using UnityEngine.UI;
 public class Body : MonoBehaviour
 {
 	public GameObject thisPanel;
+
+	public double Power { get; set; }
+	public int Lvl { get; set; }
+	public int Materials { get; set; }
+	public double Price { get; set; }
+
+
+	/**
+	 * Close Panel
+	 */
 	public void closeButt()
 	{
 		thisPanel.SetActive(false);
--- Assets/Scripts/Game/Chassis.cs
+++ Assets/Scripts/Game/Chassis.cs
@@ -6,8 +6,17 @@ using UnityEngine.UI;
 public class Chassis : MonoBehaviour
 {
 	public GameObject thisPanel;
+	public double Power { get; set; }
+	public int Lvl { get; set; }
+	public int Materials { get; set; }
+	public double Price { get; set; }
+
+	/**
+	 * Close Panel
+	 */
 	public void closeButt()
 	{
 		thisPanel.SetActive(false);
 	}
+
 }
