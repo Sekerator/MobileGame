73d1501224e75c6e4f2a1500f5572f4e86b63168
Sekerator <tagieinullaev@mail.ru>
2021-03-13 22:31:19 +0300

    Autorization

--- Assets/Scripts/Menu/Buttons.cs
+++ Assets/Scripts/Menu/Buttons.cs
@@ -1,15 +1,83 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Buttons : MonoBehaviour
 {
+	public string url = "http://mopsnet.tk/";
+	public Button play;
+	public GameObject autorizationPanel, profilePanel;
+	private void Start()
+	{
+		//Если пользователь не выполнил вход
+		if (!PlayerPrefs.HasKey("id"))
+		{
+			play.GetComponent<Button>().image.color = Color.gray;
+		}
+	}
+
 	/**
-	 * Функция для кнопки play 
+	 * Функция для кнопки играть
 	 */
     public void playButt()
 	{
-		SceneManager.LoadScene("Game");
+		if(PlayerPrefs.HasKey("id"))
+			SceneManager.LoadScene("Game");
+		else
+			autorizationPanel.SetActive(true);
+	}
+
+	/**
+	 * Функция закрытия панели авторизации
+	 */
+	public void closeAutoButt()
+	{
+		autorizationPanel.SetActive(false);
+	}
+	
+	/**
+	 * Функция закрытия панели профиля
+	 */
+	public void closeProfileButt()
+	{
+		profilePanel.SetActive(false);
+	}
+	
+	/**
+	 * Функция для кнопки профиля
+	 */
+	public void profileButt()
+	{
+		if (!PlayerPrefs.HasKey("id"))
+			autorizationPanel.SetActive(true);
+		else
+		{
+			profilePanel.SetActive(true);
+			gameObject.GetComponent<Profile>().selectInfo();
+		}
+	}
+
+	public void loginButt()
+	{
+		gameObject.GetComponent<Autorization>().login();
+	}
+
+	public void logOutButt()
+	{
+		PlayerPrefs.DeleteKey("id");
+		profilePanel.SetActive(false);
+	}
+
+	public void updateButt()
+	{
+		gameObject.GetComponent<Profile>().updateInfo();
+	}
+
+	public void registrationButt()
+	{
+		gameObject.GetComponent<Autorization>().registration();
 	}
 }
---
+++ Assets/Scripts/Menu/Profile.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SimpleJSON;
+
+public class Profile : MonoBehaviour
+{
+    private string url;
+    public InputField loginInput, passwordInput;
+
+    private void Awake()
+    {
+        Buttons butt = new Buttons();
+        url = butt.url;
+    }
+    
+    public void selectInfo()
+    {
+        StartCoroutine(profileSelectOperation("select.php"));
+    }
+    
+    public void updateInfo()
+    {
+        StartCoroutine(profileUpdateOperation("update.php"));
+    }
+
+    IEnumerator profileUpdateOperation(string operation)
+    {
+        WWWForm login = new WWWForm();
+        login.AddField("nickname", loginInput.text);
+        login.AddField("password", passwordInput.text);
+        WWW data = new WWW(url + operation, login);
+        yield return data;
+        Debug.Log(data.text);
+        JSONArray result = JSON.Parse(data.text) as JSONArray;
+        loginInput.text = result[0]["nickname"];
+        passwordInput.text = "";
+    }
+    
+    IEnumerator profileSelectOperation(string operation)
+    {
+        WWWForm login = new WWWForm();
+        login.AddField("id", PlayerPrefs.GetInt("id"));
+        WWW data = new WWW(url + operation, login);
+        yield return data;
+        JSONArray result = JSON.Parse(data.text) as JSONArray;
+        loginInput.text = result[0]["nickname"];
+        passwordInput.text = "";
+    }
+}
