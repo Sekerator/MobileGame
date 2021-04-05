f70db3bb2cbb6bb80df99b9cd09a8c771b17f3a3
Danil <danil.filippov01@mail.ru>
2021-04-05 14:12:14 +0300

    БД

--- Assets/Scripts/Menu/Buttons.cs
+++ Assets/Scripts/Menu/Buttons.cs
@@ -8,147 +8,109 @@ using UnityEngine.UI;
 
 public class Buttons : MonoBehaviour
 {
-    public InputField nickname;
-
-    private string url = "http://mopsnet.tk/startGame.php";
+    //private string url = "http://mopsnet.tk/startGame.php";
     //private string url = "http://dev.mobile.game/startGame.php";
-    //private string url = "http://localhost/startGame.php";
-    public Button playButton;
+    private string url = "http://localhost/startGame.php";
     public GameObject loadingPanel;
     public Text loadingText;
     private int countPlayers = 3;
     private float timeRemaining = 5f;
     private string layout = "Пожалуйста ожидайте\nПользователей в очереди: ";
+    
+    // Profile
+    public GameObject ProfilePanel;
+    public InputField nickname, password, updateNickname;
+    public GameObject login_butt, registration_butt, updateLogin_butt;
 
-    private void Awake()
+    public void Awake()
     {
-        PlayerPrefs.SetString("url", url);
         if (PlayerPrefs.HasKey("nickname"))
+            logined(true);
+    }
+    
+    /**
+     * Переключение панели 
+     */
+    private void logined(bool log = false)
+    {
+        if (log == false)
         {
-            nickname.text = PlayerPrefs.GetString("nickname");
-            loadingExit();
-            nickname.text = "";
-            PlayerPrefs.DeleteKey("nickname");
+            updateLogin_butt.SetActive(false);
+            GameObject dataObj = updateNickname.GetComponent<GameObject>();
+            dataObj.SetActive(false);
+            login_butt.SetActive(true);
+            registration_butt.SetActive(true);
+            dataObj = nickname.GetComponent<GameObject>();
+            dataObj.SetActive(true);
+            dataObj = password.GetComponent<GameObject>();
+            dataObj.SetActive(true);
+        }
+        else
+        {
+            updateLogin_butt.SetActive(true);
+            GameObject dataObj = updateNickname.GetComponent<GameObject>();
+            dataObj.SetActive(true);
+            login_butt.SetActive(false);
+            registration_butt.SetActive(false);
+            dataObj = nickname.GetComponent<GameObject>();
+            dataObj.SetActive(false);
+            dataObj = password.GetComponent<GameObject>();
+            dataObj.SetActive(false);
         }
     }
 
-    /**
-	 * Нажатие на кнопку играть
-	 */
-    public void playButt()
+    IEnumerator log_in()
     {
-        StartCoroutine(loading());
+        WWWForm add = new WWWForm();
+        add.AddField("request", "login_user");
+        add.AddField("nickname", nickname.text);
+        add.AddField("password", password.text);
+        WWW a = new WWW(url, add);
+        yield return a;
+        if (a.text == "Good")
+        {
+            PlayerPrefs.SetString("nickname", nickname.text);
+            logined(true);
+        }
     }
-
+    
+    IEnumerator registrat_ion()
+    {
+        WWWForm add = new WWWForm();
+        add.AddField("request", "add_user");
+        add.AddField("nickname", nickname.text);
+        add.AddField("password", password.text);
+        WWW a = new WWW(url, add);
+        yield return a;
+        if (a.text == "Good")
+        {
+            PlayerPrefs.SetString("nickname", nickname.text);
+            logined(true);
+        }
+        else
+        {
+            Debug.Log(a.text);
+        }
+    }
+    
+    public void login()
+    {
+        StartCoroutine(log_in());
+    }
+    
+    public void registration()
+    {
+        StartCoroutine(registrat_ion());
+    }
+    
     private void FixedUpdate()
     {
         timeRemaining -= Time.deltaTime;
         if (timeRemaining <= 0)
         {
-            StartCoroutine(countPlayer());
             timeRemaining = 5f;
         }
     }
-
-    /**
-	 * Проверка количества игроков и начало игры, если игроков 
-	 */
-    IEnumerator countPlayer()
-    {
-        // Получение количества пользователей в очереди
-        WWWForm form = new WWWForm();
-        form.AddField("request", "count_users");
-        WWW www = new WWW(url, form);
-        yield return www;
-
-        // Получение номера комнаты
-        WWWForm forms = new WWWForm();
-        forms.AddField("request", "get_room");
-        forms.AddField("nickname", PlayerPrefs.GetString("nickname"));
-        WWW wwws = new WWW(url, forms);
-        yield return wwws;
-
-        loadingText.text = layout + www.text;
-        if (www.text == countPlayers.ToString())
-        {
-            form = new WWWForm();
-            form.AddField("request", "start_game");
-            www = new WWW(url, form);
-            yield return www;
-        }
-        if ((wwws.text != "-1") && (wwws.text != "Error"))
-        {
-            SceneManager.LoadScene("Game");
-        }
-    }
-
-    /**
-	 * Создание пользователя в таблице users
-	 */
-    IEnumerator loading()
-    {
-        WWWForm form = new WWWForm();
-        form.AddField("request", "add_user");
-        form.AddField("nickname", nickname.text);
-        WWW www = new WWW(url, form);
-        yield return www;
-        if (www.text != "Error")
-        {
-            playButton.enabled = false;
-            loadingPanel.SetActive(true);
-            loadingText.text += www.text;
-            PlayerPrefs.SetString("nickname", nickname.text);
-        }
-    }
-
-    /**
-	 * Нажатие на кнопку выхода на панели
-	 */
-    public void exitButt()
-    {
-        StartCoroutine(loadingExit());
-    }
-
-    /**
-	 * Удаление пользователя из таблицы users
-	 */
-    IEnumerator loadingExit()
-    {
-        loadingText.text = layout;
-        playButton.enabled = true;
-        loadingPanel.SetActive(false);
-        WWWForm form = new WWWForm();
-        form.AddField("request", "delete_user");
-        form.AddField("nickname", nickname.text);
-        if (PlayerPrefs.HasKey("nickname"))
-            PlayerPrefs.DeleteKey("nickname");
-        WWW www = new WWW(url, form);
-        yield return www;
-    }
-
-    private void OnApplicationQuit()
-    {
-        StartCoroutine(loadingExit());
-        PlayerPrefs.DeleteAll();
-    }
-
-    private void OnApplicationPause(bool status)
-    {
-        if (status == true)
-        {
-            StartCoroutine(loadingExit());
-            PlayerPrefs.DeleteAll();
-            Application.Quit();
-        }
-    }
     
-    private void OnApplicationFocus(bool status)
-    {
-        if (status == false)
-        {
-            StartCoroutine(loadingExit());
-            PlayerPrefs.DeleteAll();
-            Application.Quit();
-        }
-    }
+    
 }
